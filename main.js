436c8e37a6a33f09c364a3af3270a136d79c9ec3
Inun572 <helmi.ainun@gmail.com>
2023-12-13 16:22:10 +0700

    refactor html to component

--- main.js
+++ main.js
@@ -1,47 +1,15 @@
 import './style.css';
-import instagram from './assets/img/instagram_6422200.png';
-import github from './assets/img/github-sign.png';
-import youtube from './assets/img/youtube_6422215.png';
 
 import { footer } from './components/footer';
+import { profile } from './components/profile';
+import { social } from './components/social';
+import { links } from './components/links';
 
 document.querySelector('#app').innerHTML = `<div id="root">
 <div class="wrapper">
-  <div class="profile">
-    <img src="https://avatar.iran.liara.run/public/boy" alt="avatar" />
-    <h3 class="name">Helmi Ainun</h3>
-    <h6 class="title">Junior Front End Developer</h6>
-  </div>
-  <div class="social">
-    <a href="https://instagram.com/helmiainun" target="_blank">
-      <img src=${instagram} alt="instagram" />
-    </a>
-    <a
-      href="https://www.youtube.com/channel/UCV6lZ1qluM95bZCnxl6_lmg"
-      target="_blank"
-    >
-      <img src=${youtube} alt="youtube" />
-    </a>
-    <a href="https://github.com/helmiainun" target="_blank">
-      <img src=${github} alt="github" />
-    </a>
-  </div>
-  <div class="links">
-    <a
-      class="link"
-      href="http://helmi-ainun-portfolio.vercel.app/"
-      target="_blank"
-    >
-      My Portfolio
-    </a>
-    <a class="link" href="https://www.linkedin.com/in/helmiainunnaim/"
-    target="_blank"
-    >
-        LinkedIn Profile
-    </a>
-    <a class="link" href="#"> Aja aja ada </a>
-    <a class="link" href="#"> Belum ketawa udah apa apa </a>
-  </div>
+  ${profile()}
+  ${social()}
+  ${links()}  
   ${footer()}
 </div>
 </div>`;
