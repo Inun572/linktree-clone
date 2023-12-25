e502f4af680d10919aed9e8b729cb469addaf7d7
Inun572 <helmi.ainun@gmail.com>
2023-12-26 06:31:16 +0700

    -update photo profile

--- components/profile.js
+++ components/profile.js
@@ -1,6 +1,6 @@
 export const profile = () => {
   return `<div class="profile">
-    <img src="https://avatar.iran.liara.run/public/boy" alt="avatar" />
+    <img src="../assets/img/profile2_square.jpg" alt="avatar" />
     <h3 class="name">Helmi Ainun</h3>
     <h6 class="title">Junior Front End Developer</h6>
   </div>`;
