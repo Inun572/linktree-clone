50988a1838a38b5884d8df7863654847e1c94f11
Inun572 <helmi.ainun@gmail.com>
2023-12-26 06:38:20 +0700

    fix src img for photo profile

--- components/profile.js
+++ components/profile.js
@@ -1,6 +1,8 @@
+import photo from '../assets/img/profile2_square.jpg';
+
 export const profile = () => {
   return `<div class="profile">
-    <img src="../assets/img/profile2_square.jpg" alt="avatar" />
+    <img src=${photo} alt="avatar" />
     <h3 class="name">Helmi Ainun</h3>
     <h6 class="title">Junior Front End Developer</h6>
   </div>`;
